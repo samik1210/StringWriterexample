46ed232c652fe648c888ed0073633c7c5a8bd332
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:19:52 +0530

    Add project files.

---
+++ StringWriterexample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StringWriterexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string text = "hii parag \n" +
+                "welcome to my home. \n" +
+                "hii samiksha";
+            StringBuilder sb = new StringBuilder();  
+            StringWriter writer = new StringWriter(sb);  
+            writer.WriteLine(text);
+            writer.Flush();  
+            writer.Close();  
+            StringReader reader = new StringReader(sb.ToString()); 
+            while (reader.Peek() > -1)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+    
+}
